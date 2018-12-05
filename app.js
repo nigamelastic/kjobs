e9840275c4aa96dddca9f86cca4bf2399ea334b3
nigamelastic <nigamelastic@gmail.com>
2018-12-05 13:59:42 +0100

    first draft for heroku

--- app.js
+++ app.js
@@ -15,19 +15,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
-    let url = `https://indreed.herokuapp.com/api/jobs?q=qa+tester+automation&limit=50`;
-    axios({
-        method: 'get',
-        url
+    res.render("index", {title:"KJobs"});
+});
+
+
+app.get('/search', function (req, res) {
+    queries = req.query;
+    let url = `https://indreed.herokuapp.com/api/jobs`;
+    if (queries){
+        axios.get(url, {
+        params: queries
     })
-    .then(function (response) {
-        let jobs = response.data;
-        res.render("index", { title: "Jobby", jobs: jobs});
+    .then(function(response){
+        res.render("search", { title: "KJobs", jobs: response.data});
     })
-    .catch(function (error) {
+    .catch(function(error) {
         console.log(error);
     });
-
+    }
+    else {
+        res.render("search", {title: "KJobs"})
+    }
 });
 
 app.listen(app.get('PORT'), function () {
